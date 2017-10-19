d8e313cf8f4ca9117c617408e582d3da2b54f2cc
slp423 <penningtonfamily04@gmail.com>
2017-10-18 23:06:07 -0600

    Added for loop that steps through and periodically adds a "*" character ultamatly displaying a pyramid.

--- DisplayPyramid_Console/Program.cs
+++ DisplayPyramid_Console/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DisplayPyramid_Console
 {
     class Program
-    {
+    {   
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var userInput = Int32.Parse(Console.ReadLine());
+            DrawPyramid(userInput);
+        }
+        static void DrawPyramid(int n)
+        {
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = i; j <= n; j++)
+                    {
+                        Console.Write("  ");
+                    }
+                    for (int k = 1; k <= 2 * i - 1; k++)
+                    {
+                        Console.Write("*" + " ");
+                    }
+                    Console.WriteLine();
+                }
+            Console.ReadLine();
         }
     }
 }
